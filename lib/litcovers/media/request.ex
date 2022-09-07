21e357e2a39f8b5de41b3fdd9fa708798dd8e85e
Kitasio <soulgen@protonmail.com>
2022-09-07 13:04:02 +0300

    refined request form

--- lib/litcovers/media/request.ex
+++ lib/litcovers/media/request.ex
@@ -10,13 +10,24 @@ defmodule Litcovers.Media.Request do
     field :status, :string
     field :title, :string
     field :vibe, :string
-    field :user_id, :id
+    
+    belongs_to :user, Litcovers.Accounts.User
 
     timestamps()
   end
 
   @doc false
   def changeset(request, attrs) do
+    request
+    |> cast(attrs, [:author, :title, :vibe, :description, :genre])
+    |> validate_required([:author, :title, :vibe, :description, :genre])
+    |> validate_length(:author, max: 20)
+    |> validate_length(:title, max: 30)
+    |> validate_length(:vibe, max: 40)
+    |> validate_length(:description, min: 20, max: 255)
+  end
+
+  def admin_changeset(request, attrs) do
     request
     |> cast(attrs, [:status, :max, :author, :title, :vibe, :description, :genre])
     |> validate_required([:status, :max, :author, :title, :vibe, :description, :genre])
