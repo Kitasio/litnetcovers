cf9a3905358b7be299d8fd4102ae8b8b056614eb
Kitasio <soulgen@protonmail.com>
2022-10-04 13:59:02 +0400

    ai code added

---
+++ lib/sd_params.ex
@@ -0,0 +1,12 @@
+defmodule SDParams do
+  @derive Jason.Encoder
+  defstruct [
+      version: "a9758cbfbd5f3c2094457d996681af52552901775aa2d6dd0b17fd15df959bef",
+      input: %{
+        prompt: "multicolor hyperspace",
+        width: 512,
+        height: 704,
+        num_outputs: 1
+      }
+  ]
+end
