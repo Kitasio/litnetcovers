1688a29b22484a490da20689305c1599c395e875
Kitasio <soulgen@protonmail.com>
2022-11-29 19:48:14 +0400

    oai new model

--- lib/oai_params.ex
+++ lib/oai_params.ex
@@ -2,6 +2,6 @@ defmodule OAIParams do
   @derive Jason.Encoder
   defstruct prompt: "Hello mr robot",
             max_tokens: 255,
-            model: "text-davinci-002",
+            model: "text-davinci-003",
             temperature: 1
 end
